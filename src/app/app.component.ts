434ed6befa27e98bf1e702a24527fa2d353305c5
cintia <ccastelgonzalez@safareyes.es>
2025-05-06 19:31:08 +0200

    ♻️Actualización app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from './services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ContactoComponent } from './contacto/contacto.component';
+import { EventoComponent } from './evento/evento.component';
+import {InscripcionComponent} from './inscripcion/inscripcion.component';
+import {ClienteComponent} from './cliente/cliente.component';
+import {EventoAdminComponent} from './evento-admin/evento-admin.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    // HomeComponent,
+    CommonModule,
+    ReactiveFormsModule,
+    // LoginComponent,
+    // RegisterComponent,
+    // ContactoComponent,
+    // EventoComponent,
+    // InscripcionComponent,
+    // ClienteComponent,
+    // EventoAdminComponent,
+  ],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'granjaGaiaFront';
+  title = 'GranjaGaia';
+
+  constructor(private authService: AuthService) {}
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
-import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import {Routes} from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {ContactoComponent} from './contacto/contacto.component';
+import {EventoComponent} from './evento/evento.component';
+import {InscripcionComponent} from './inscripcion/inscripcion.component';
+import {ClienteComponent} from './cliente/cliente.component';
+import {EventoAdminComponent} from './evento-admin/evento-admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'contact', component: ContactoComponent },
+  { path: 'events', component: EventoComponent },
+  { path: 'adminevent', component: EventoAdminComponent },
+
+  { path: 'inscripcion/:id', component: InscripcionComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Ruta por defecto
+  { path: '**', redirectTo: '/home' },
+  { path: 'cliente', component: ClienteComponent },
+
+
+
+];
