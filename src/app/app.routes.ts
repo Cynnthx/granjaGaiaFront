4647b68a01e045997b8ceb08a72c9c4669115425
cintia <ccastelgonzalez@safareyes.es>
2025-05-07 17:50:51 +0200

    ♻️Actualización rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { HomeComponent } from './home/home.component';
-import {Routes} from '@angular/router';
-import {LoginComponent} from './login/login.component';
-import {RegisterComponent} from './register/register.component';
-import {ContactoComponent} from './contacto/contacto.component';
-import {EventoComponent} from './evento/evento.component';
-import {InscripcionComponent} from './inscripcion/inscripcion.component';
-import {ClienteComponent} from './cliente/cliente.component';
-import {EventoAdminComponent} from './evento-admin/evento-admin.component';
+import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { EventoComponent } from './evento/evento.component';
+import { InscripcionComponent } from './inscripcion/inscripcion.component';
+import { ClienteComponent } from './cliente/cliente.component';
+import { EventoAdminComponent } from './evento-admin/evento-admin.component';
+import { TiendaComponent } from './tienda/tienda.component';
+// import { ProductoDetalleComponent } from './producto-detalle/producto-detalle.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -15,12 +17,10 @@ export const routes: Routes = [
   { path: 'contact', component: ContactoComponent },
   { path: 'events', component: EventoComponent },
   { path: 'adminevent', component: EventoAdminComponent },
-
   { path: 'inscripcion/:id', component: InscripcionComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Ruta por defecto
-  { path: '**', redirectTo: '/home' },
   { path: 'cliente', component: ClienteComponent },
-
-
-
+  { path: 'tienda', component: TiendaComponent },
+  // { path: 'producto/:id', component: ProductoDetalleComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Ruta por defecto
+  { path: '**', redirectTo: '/home' }
 ];
