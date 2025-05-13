1504d950f7d683deaf46c74149b68e5948aa259d
cintia <ccastelgonzalez@safareyes.es>
2025-05-13 18:54:43 +0200

    ♻️Actualización de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,8 @@ import { InscripcionComponent } from './inscripcion/inscripcion.component';
 import { ClienteComponent } from './cliente/cliente.component';
 import { EventoAdminComponent } from './evento-admin/evento-admin.component';
 import { TiendaComponent } from './tienda/tienda.component';
+import {CategoriaComponent} from './categoria/categoria.component';
+import {TiendaAdminComponent} from './tienda-admin/tienda-admin.component';
 // import { ProductoDetalleComponent } from './producto-detalle/producto-detalle.component';
 
 export const routes: Routes = [
@@ -20,7 +22,10 @@ export const routes: Routes = [
   { path: 'inscripcion/:id', component: InscripcionComponent },
   { path: 'cliente', component: ClienteComponent },
   { path: 'tienda', component: TiendaComponent },
-  // { path: 'producto/:id', component: ProductoDetalleComponent },
+  { path: 'categorias', component: CategoriaComponent },
+  { path: 'admintienda', component: TiendaAdminComponent },
+
+// { path: 'producto/:id', component: ProductoDetalleComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // Ruta por defecto
   { path: '**', redirectTo: '/home' }
 ];
